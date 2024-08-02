9a2c5e86be1da8b3f4758bb4db76910f354f4948
kathyhu91 <huchiachun@gmail.com>
2024-08-02 23:08:28 +0800

    feat(PageNotFoundView): add 404 page and set up redirect navigation

---
+++ src/views/PageNotFoundView.vue
@@ -0,0 +1,39 @@
+<style lang="scss" scoped>
+.guide {
+  &__wrap {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  }
+}
+</style>
+<template>
+  <div class="guide__wrap">
+    <p class="heading-h3 tc-danger">{{ lostGuideConfig.icon }}</p>
+    <h2 class="heading-h5 tc-danger">{{ lostGuideConfig.msg }}</h2>
+    <RouterLink class="btn btn-primary" :to="lostGuideConfig.entranceLink">{{
+      lostGuideConfig.btn
+    }}</RouterLink>
+  </div>
+</template>
+<script setup>
+import { onMounted } from 'vue'
+import { useRouter, RouterLink } from 'vue-router'
+import { navMenu } from '@/utils/navUtils.js'
+const lostGuideConfig = {
+  icon: '( ºωº )',
+  msg: 'Oops! Seems like we’ve lost our way!',
+  btn: 'Return to Index',
+  entranceLink: navMenu.find((item) => item.title === 'gate').to
+}
+
+const router = useRouter()
+onMounted(() => {
+  setTimeout(() => {
+    router.push(lostGuideConfig.entranceLink)
+  }, 3000)
+})
+</script>
